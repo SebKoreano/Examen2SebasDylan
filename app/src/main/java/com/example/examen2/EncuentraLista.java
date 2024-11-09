640c0a4d3fe707c06133a0d678c4e2e42c1be35f
MocaPan <d.mora.5@estudiantec.cr>
2024-11-09 14:24:29 -0600

    [ADD] Método encuentra lista
    se creó el método encuentra lista

---
+++ app/src/main/java/com/example/examen2/EncuentraLista.java
@@ -0,0 +1,12 @@
+package com.example.examen2;
+
+import java.util.List;
+
+public class EncuentraLista {
+
+    //Método para encontrar si un elemento entero está en una lista de elementos enteros.
+
+    public static boolean Encuentra(List<Integer> lista, int elemento) {
+        return lista.contains(elemento);
+    }
+}
